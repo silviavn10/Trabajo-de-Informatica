12fbfc81638412bdaa4f1de56d600b5bc431a66a
AndreaMC7 <a.magro@alumnos.upm.es>
2020-05-06 11:21:00 +0200

    Creditos

--- XtremETSIDI/XtremETSIDI/src/Creditos.cpp
+++ XtremETSIDI/XtremETSIDI/src/Creditos.cpp
@@ -10,6 +10,10 @@ Creditos::Creditos() : SpriteCreditos("imagenes/credito.png", 8) //Constructor
 	velocidad.y = 0; //FALTAN VALORES BUENOS. Velocidad vertical del bonus
 	posicion.x = 8;
 	posicion.y = 4.5f;
+	//limite1.x = posicion.x - (lado / 2);
+	//limite1.y = posicion.y + (lado / 2);
+	//limite2.x = posicion.x + (lado / 2);
+	//limite2.y = posicion.y + (lado / 2);
 }
 
 Creditos::~Creditos() //Destructor
@@ -27,4 +31,24 @@ Creditos::Creditos(float rad, float x, float y, float vx, float vy) : SpriteCred
 
 	SpriteCreditos.setCenter(0.5, 0.5);  //Centro del sprite para la vida
 	SpriteCreditos.setSize(1, 1);  //Tamaño del sprite para la vida
+}
+
+float Creditos::distancia(Vector2D punto, Vector2D* direccion)
+{
+	Vector2D u = (punto - limite1);
+	Vector2D v = (limite2 - limite1).Unitario();
+	float longitud = (limite1 - limite2).modulo();
+	Vector2D dir;
+	float valor = u * v;
+	float distancia = 0;
+	if (valor < 0)
+		dir = u;
+	else if (valor > longitud)
+		dir = (punto - limite2);
+	else
+		dir = u - v * valor;
+	distancia = dir.modulo();
+	if (direccion != 0)
+		*direccion = dir.Unitario();
+	return distancia;
 }
--- XtremETSIDI/XtremETSIDI/src/Creditos.h
+++ XtremETSIDI/XtremETSIDI/src/Creditos.h
@@ -9,10 +9,13 @@ class Creditos
 public:
 	friend class Interaccion;
 	Creditos();
-	Creditos(float rad, float x = 0, float y = 0, float vx = 0, float vy = 0);
 	~Creditos();
+	Creditos(float rad, float x = 0, float y = 0, float vx = 0, float vy = 0);
+	float distancia(Vector2D punto, Vector2D* direccion = 0);
 	Vector2D velocidad;
 	Vector2D posicion;
+	//Vector2D limite1;
+	//Vector2D limite2;
 	float radio;
 private:
 	SpriteSequence SpriteCreditos;
